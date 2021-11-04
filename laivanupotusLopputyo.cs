ea65fe87c1855bb13c0e03e8aee082b31f046c7f
gr200947 <91872812+gr200947@users.noreply.github.com>
2021-11-04 11:22:46 +0200

    Add files via upload

---
+++ laivanupotusLopputyo.cs
@@ -0,0 +1,121 @@
+using System;
+
+public class Class1
+{
+    public static int peliX;
+  public static int peliY;
+  public static void Main()
+  {
+    int x, y,botX,botY, syoteX, syoteY;
+    Random newRandom = new Random();
+
+    botX = newRandom.Next(1, 5);
+    botY = newRandom.Next(1, 5);
+    start();
+
+    //voidissa kysytään laivan kordinaatit
+    void start()
+    {
+      //kysytään kordinaatit
+      Console.WriteLine("Anna laivan X kordinaatti, Pelilauta on 5 x 5");
+      syoteX = int.Parse(Console.ReadLine());
+      if (syoteX < 6 && syoteX > 0)
+      {
+        x = syoteX;
+      }
+      else
+      {
+        Console.WriteLine("Pelilaudan koko on 5 x 5, anna pienempi arvo");
+        syoteX = int.Parse(Console.ReadLine());
+        start();
+      }
+
+      Console.WriteLine(" ");
+      Console.WriteLine("Anna laivan Y kordinaatti");
+      syoteY = int.Parse(Console.ReadLine());
+
+      if (syoteY < 6 && syoteY > 0)
+      {
+        y = syoteY;
+        peli();
+
+      }
+      else
+      {
+        Console.WriteLine("Pelilaudan koko on 5 x 5, anna pienempi arvo");
+        syoteY = int.Parse(Console.ReadLine());
+        start();
+      }
+    }
+     //botin tulitus
+    void botTulitus()
+    {
+      int newBotX, newBotY;
+      Random randomEl = new Random();
+      newBotX = randomEl.Next(1, 5);
+      newBotY = randomEl.Next(1, 5);
+
+      if(newBotX==x && newBotY == y)
+      {
+        Console.WriteLine("Laivaasi osui, Hävisit pelin");
+      }
+      else
+      {
+        Console.WriteLine(" ");
+        Console.WriteLine("Vastustaja ei osunut, sinun vuorosi");
+        peli();
+      }
+
+    }
+
+    //pelaajan tulitus
+    void peli()
+    {
+      if (peliX == botX && peliY == botY)
+      {
+        Console.WriteLine("Voitit pelin");
+      }
+      else {
+       
+
+        int tryY,tryX;
+        Console.WriteLine("Anna X kordinaatti jota tulitetaan");
+        tryX = int.Parse(Console.ReadLine());
+
+        if (tryX < 6 && tryX > 0)
+        {
+          peliX = tryX;
+        }
+        else
+        {
+          Console.WriteLine("Pelilaudan koko on 5 x 5, anna pienempi arvo");
+          peli();
+        }
+
+        Console.WriteLine(" ");
+        Console.WriteLine("Anna Y kordinaatti jota tulitetaan");
+        tryY = int.Parse(Console.ReadLine());
+
+        if (tryY < 6 && tryY > 0)
+        {
+          peliY = tryY;
+        
+
+        }
+        else
+        {
+         Console.WriteLine("Pelilaudan koko on 5 x 5, anna pienempi arvo");
+          peli();
+        }
+
+          Console.WriteLine("Ei osumia");
+          botTulitus();
+
+      }
+
+
+    }
+  
+  }
+
+}
---
+++ nopanheittoLopputyo.cs
@@ -0,0 +1,31 @@
+using System;
+
+public class Class1
+{
+  public static int total;
+	public static void Main()
+	{
+    Random rnd = new Random();
+    int noppa = rnd.Next(1, 7);
+    
+    int noppa1;
+    int noppa2;
+      for (int i = 0; i < 5; i++)
+      {
+      Console.WriteLine(" ");
+
+      Console.WriteLine("Heitä noppaa painamalla näppäintä");
+      Console.ReadKey();
+      noppa1 = rnd.Next(1, 7);
+      noppa2 = rnd.Next(1, 7);
+      total += noppa1;
+      total += noppa2;
+      Console.WriteLine("Nopan1 silmäluku: " + noppa1);
+      Console.WriteLine("Nopan2 silmäluku: " + noppa2);
+      
+      }
+    Console.WriteLine(" ");
+    Console.WriteLine("Kaikkien heittojen summa: "+total);
+
+  }
+}
